10cac4883dca5f1eae82e47fc59a86b1c8e40407
RamiQuebec <rami.kabak25@ycdsbk12.ca>
2024-09-13 18:47:38 -0400

    Review4.java Finishing Touches!

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,29 @@
 package gr11review.part1;
+import java.io.*;
+import java.text.DecimalFormat;
+
+public class Review4 {
+    public static void main(String[] args) throws IOException {
+        int intNumItems;
+        double dblSubtotal = 0;
+        double dblTax;
+        double dblTotal;
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("How many items do you want to buy? ");
+        intNumItems = Integer.parseInt(keyboard.readLine());
+        
+        for (int i = 1; i < intNumItems + 1; i++) {
+            System.out.print("Enter the price for item " + i + ": ");
+            dblSubtotal = dblSubtotal + Double.parseDouble(keyboard.readLine());
+        }
+
+        dblTax = dblSubtotal * 0.13;
+        dblTotal = dblSubtotal + dblTax;
+        DecimalFormat format = new DecimalFormat("##.00");
+
+        System.out.println("Subtotal: " + "$" + dblSubtotal);
+        System.out.println("Tax: " + "$" + format.format(dblTax));
+        System.out.println("Total: " + "$" + format.format(dblTotal));
+    }
+}
