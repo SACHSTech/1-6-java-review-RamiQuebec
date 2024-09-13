90714767bd101804a2a23641147a2079e9f58470
RamiQuebec <rami.kabak25@ycdsbk12.ca>
2024-09-13 18:16:59 -0400

    Review3.java Done!

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,18 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review3 {
+    public static void main(String[] args) throws IOException {
+        for (int i = 20; i < 101; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+
+        for (int i = 29; i > 0; i--) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
