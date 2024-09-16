6a0f89fb3b29727578e81a60113b080ee62aefa6
RamiQuebec <rami.kabak25@ycdsbk12.ca>
2024-09-16 18:58:42 -0400

    Review6.java done, passes JUnit test!

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,1 +1,28 @@
 package gr11review.part1;
+import java.io.*;
+import java.text.DecimalFormat;
+
+public class Review6 {
+    public static void main(String[] args) throws IOException {
+        double dblLoopCheck = 1;
+        double dblSubtotal = 0;
+        double dblTax;
+        double dblTotal;
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        while (dblLoopCheck != 0) {
+            System.out.print("Enter the price for an item: ");
+            dblLoopCheck = Double.parseDouble(keyboard.readLine());
+            dblSubtotal = dblSubtotal + dblLoopCheck;
+        }
+        
+        dblTax = dblSubtotal * 0.13;
+        dblTotal = dblSubtotal + dblTax;
+        DecimalFormat format = new DecimalFormat("#0.00");
+
+
+        System.out.println("Subtotal: " + "$" + format.format(dblSubtotal));
+        System.out.println("Tax: " + "$" + format.format(dblTax));
+        System.out.println("Total: " + "$" + format.format(dblTotal));
+    }
+}
