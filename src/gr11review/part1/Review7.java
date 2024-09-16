05aa373a18c4516c7737c7ac1a7972f262d015b1
RamiQuebec <rami.kabak25@ycdsbk12.ca>
2024-09-16 19:15:19 -0400

    Review7 W.I.P missing last part

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,30 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review7 {
+    public static void main(String[] args) throws IOException {
+        String theSentence;
+        int intSpaces = 0;
+        int intaCount = 0;
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        theSentence = keyboard.readLine();
+
+        for (int i = 0; i < theSentence.length(); i++) {
+            if (Character.isWhitespace(theSentence.charAt(i))) {
+                intSpaces++;
+            }
+        }
+
+        for (int i = 0; i < theSentence.length(); i++) {
+            if (theSentence.charAt(i) == 'a') {
+                intaCount++;
+            }
+        }
+
+        System.out.println("There are " + theSentence.length() + " characters in the sentence.");  
+        System.out.println("There are " + intSpaces + " spaces in the sentence.");  
+        System.out.println("There are " + intaCount + " letter a in the sentence.");  
+
+    }
+}
